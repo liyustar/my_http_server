a8a80d6661c104be8caee99e8c40d7e5d03119b9
Li Yuxing <liyustar@gmail.com>
2013-01-09 16:50:26 +0800

    add multi-process

--- src/httpd.cpp
+++ src/httpd.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -12,6 +13,8 @@ using namespace std;
 
 #include "SocketUtility.h"
 #include "HttpUtility.h"
+
+#define MAX_PROCESS 5
 
 void print_ascii(char *str);
 
@@ -28,27 +31,37 @@ int main(int argc, char *argv[]) {
 
 	int servSock = SetupTCPServerSocket(argv[1]);
 
-	for (;;) {	// Run forever
-		int clntSock = AcceptTCPConnection(servSock);
+	for (int i=0; i<MAX_PROCESS-1; i++) {	// Run forever
+		int pid;
+		if( (pid = fork()) == -1)
+			perror("fork error");
+		else if (pid != 0 && i<MAX_PROCESS-2)
+			continue;
 
-		// read request
-		Http_message request;
-		char buf[BUFSIZ];
-		FILE *fpin = fdopen(clntSock, "r");
-		fgets(buf, BUFSIZ, fpin);
-		request.parseStartLine(buf);			// request start line
-		while( fgets(buf, BUFSIZ, fpin) )		// request headers
-		{
-			if(0 == strcmp("\r\n", buf)) break;
-			request.parseHeader(buf);
+		while(1) {
+			int clntSock = AcceptTCPConnection(servSock);
+
+			// read request
+			Http_message request;
+			char buf[BUFSIZ];
+			FILE *fpin = fdopen(clntSock, "r");
+			fgets(buf, BUFSIZ, fpin);
+			request.parseStartLine(buf);			// request start line
+			while( fgets(buf, BUFSIZ, fpin) )		// request headers
+			{
+				if(0 == strcmp("\r\n", buf)) break;
+				request.parseHeader(buf);
+			}
+			printf("|EOF|\n");
+
+			// response
+			// cout << request.buildMsgHeader() << "|build finish|" << endl;
+			process_rq(request, clntSock);
+
+			fclose(fpin);
 		}
-		printf("|EOF|\n");
 
-		// response
-		// cout << request.buildMsgHeader() << "|build finish|" << endl;
-		process_rq(request, clntSock);
-
-		fclose(fpin);
+		exit(0);
 	}
 
 	return 0;	// Not Reached
